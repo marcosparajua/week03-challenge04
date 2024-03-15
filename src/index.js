7df935703b166fbea3966a72ce270facbb123d77
marcos parajua <marcosparajua@gmail.com>
2024-03-15 08:36:38 +0100

    add card list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@
 /* eslint-disable no-unused-vars */
 import { Component } from './component';
 import { Header } from './header';
+import { Main } from './main';
+import { Card } from './card';
+import { series } from './data';
 import './style.css';
 
 new Header('.main-title', 'My Series');
+new Main('.main');
+series.forEach((serie) => {
+  const { name, creator, year, poster } = serie;
+
+  new Card('.container', name, creator, year, poster);
+});
